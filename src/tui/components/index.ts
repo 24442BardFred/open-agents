dcfec09ee77f9ede6b1d3b4d9f922f2ef525734d
nicoalbanese <gcalbanese96@gmail.com>
2026-01-04 15:42:49 +0100

    Export StandaloneTodoList from components

--- src/tui/components/index.ts
+++ src/tui/components/index.ts
@@ -1,6 +1,6 @@
 export { ToolCall } from "./tool-call.js";
 export { TextOutput } from "./text-output.js";
-export { StatusBar } from "./status-bar.js";
+export { StatusBar, StandaloneTodoList } from "./status-bar.js";
 export { InputBox } from "./input-box.js";
 export { DiffView, parseEditOutput } from "./diff-view.js";
 export { Header } from "./header.js";
